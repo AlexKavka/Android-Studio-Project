d26bcc2a5c6cff2358141b7f518229e93d01580c
AlexKavka <lilalexyar2002@gmail.com>
2026-04-03 19:59:58 +0300

    UI-lesson: homework done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,5 +57,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
